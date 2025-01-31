80efc72922a2410ccd9326122ca357a261df91b1
Lucas Torres <lucasan37@gmail.com>
2025-01-31 13:28:37 -0500

    Get date from global var

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,14 @@
 export const CONFIG = {
-    // Program configuration
-    PROGRAM_START_DATE: new Date(Date.UTC(2025, 0, 30)), // January 30, 2025 00:00:00 UTC
+    // Use a global variable if defined, otherwise default to January 1st of the current year
+    PROGRAM_START_DATE: new Date(window.INITIAL_PROGRAM_START_DATE) || new (Date().getFullYear(), 0, 1),
+
+    // Use the start date variable to calculate the end date
     get PROGRAM_END_DATE() {
-        const endDate = new Date(Date.UTC(2025, 0, 30)); // Create fresh UTC date
-        endDate.setUTCDate(endDate.getUTCDate() + 364); // Add days in UTC
+        console.log('🟩 [10] Global date', window.INITIAL_PROGRAM_START_DATE);
+        console.log('🟩 [10] PROGRAM_END_DATE', this.PROGRAM_START_DATE);
+        const endDate = new Date(this.PROGRAM_START_DATE);
+        endDate.setUTCDate(endDate.getUTCDate() + 364); // 365 days - 1 since we count the start date
+        console.log('🟩 [10] PROGRAM_END_DATE', endDate);
         return endDate;
     },
     
